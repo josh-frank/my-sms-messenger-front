4dfecb11855a896ad917bb071d70ea57bebfdd7b
Josh Frank <eurosewusa@gmail.com>
2025-07-25 18:20:51 -0400

    adding session_id with uuid and CookieService

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+
 import { MessageForm } from './message-form/message-form';
 import { MessageDisplay } from './message-display/message-display';
 
@@ -10,9 +12,25 @@ import { MessageDisplay } from './message-display/message-display';
     MessageForm,
     MessageDisplay,
   ],
+  providers: [
+    CookieService,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  
   protected readonly title = signal('my-sms-messenger');
+  private getSessionIdFromCookies;
+  private cookieService = inject(CookieService);
+
+  constructor() {
+    this.getSessionIdFromCookies = this.cookieService.get( 'session_id' );
+    if ( !this.getSessionIdFromCookies ) {
+      const uuid = crypto.randomUUID();
+      this.cookieService.set( 'session_id', uuid );
+      this.getSessionIdFromCookies = uuid;
+    }
+    // console.log( this.getSessionIdFromCookies )
+  }
 }
