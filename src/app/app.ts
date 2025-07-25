6f5e906ed159d3692b9e2715a32f7dabcedd317f
Josh Frank <eurosewusa@gmail.com>
2025-07-25 16:55:12 -0400

    adding message-display

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MessageForm } from './message-form/message-form';
+import { MessageDisplay } from './message-display/message-display';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MessageForm],
+  imports: [
+    RouterOutlet,
+    MessageForm,
+    MessageDisplay,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/message-display/message-display.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-display',
+  imports: [],
+  templateUrl: './message-display.html',
+  styleUrl: './message-display.css'
+})
+export class MessageDisplay {
+
+}
