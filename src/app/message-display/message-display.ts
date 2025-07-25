84765a4bfe37e9a7dd3476f6a2ee256e6c0c790a
Josh Frank <eurosewusa@gmail.com>
2025-07-25 17:52:12 -0400

    successfully fetching from back end with HttpClient and Observable

--- src/app/message-display/message-display.ts
+++ src/app/message-display/message-display.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  inject
+} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-message-display',
@@ -6,6 +12,20 @@ import { Component } from '@angular/core';
   templateUrl: './message-display.html',
   styleUrl: './message-display.css'
 })
-export class MessageDisplay {
+export class MessageDisplay implements OnInit {
+
+  private http = inject(HttpClient);
+  private apiUrl = 'http://104.237.150.105/messages';
+  private messages: string[] = [];
+
+  getMessages(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl, { params: { session_id: 'test' } } );
+  }
+
+  ngOnInit(): void {
+    this.getMessages().subscribe( data => {
+      this.messages = data;
+    } );
+  }
 
 }
