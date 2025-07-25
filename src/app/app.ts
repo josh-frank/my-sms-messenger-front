38b1d01139119ca5e1e5e46754dff52315ac0085
Josh Frank <eurosewusa@gmail.com>
2025-07-25 15:51:45 -0400

    adding app-message-form

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MessageForm } from './message-form/message-form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MessageForm],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/message-form/message-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-form',
+  imports: [],
+  templateUrl: './message-form.html',
+  styleUrl: './message-form.css'
+})
+export class MessageForm {
+
+}
