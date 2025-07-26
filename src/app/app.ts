289f117b941ebe321b65b36b38d752ec63dc9c66
Josh Frank <josh_frank@icloud.com>
2025-07-25 20:44:09 -0400

    fixing uuidv4()

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
+import { v4 as uuidv4 } from 'uuid';
 
 import { MessageForm } from './message-form/message-form';
 import { MessageDisplay } from './message-display/message-display';
@@ -27,7 +28,7 @@ export class App {
   constructor() {
     this.getSessionIdFromCookies = this.cookieService.get( 'session_id' );
     if ( !this.getSessionIdFromCookies ) {
-      const uuid = crypto.randomUUID();
+      const uuid = uuidv4();
       this.cookieService.set( 'session_id', uuid );
       this.getSessionIdFromCookies = uuid;
     }
